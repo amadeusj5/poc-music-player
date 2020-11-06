81e9188f48587473ca4d062fb41da72e55c25208
amadeusj5 <amadeusjosue5@gmail.com>
2020-11-05 20:38:09 -0600

    Adding Howler plugin and manipulate progress, play, pause and select specific time song

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,204 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container mx-auto my-5">
+      <!-- component -->
+      <div class="w-full">
+        <div class="h-2 bg-red-light"></div>
+        <div class="flex items-center justify-center h-screen bg-red-lightest">
+          <div
+            class="bg-white shadow-lg rounded-lg"
+            style="width: 45rem !important;"
+          >
+            <div class="flex">
+              <div>
+                <img
+                  class="w-full rounded hidden md:block"
+                  src="https://tailwindcss.com/img/card-top.jpg"
+                  alt="Album Pic"
+                />
+              </div>
+              <div class="w-full p-8">
+                <div class="flex justify-between">
+                  <div>
+                    <h3 class="text-2xl text-grey-darkest font-medium">
+                      A Sky Full of Stars
+                    </h3>
+                    <p class="text-sm text-grey mt-1">Ghost Stories</p>
+                  </div>
+                  <div class="text-red-lighter">
+                    <svg
+                      class="w-6 h-6"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div class="flex justify-between items-center mt-8">
+                  <div class="text-grey-darker">
+                    <svg
+                      class="w-8 h-8"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        d="M6.59 12.83L4.4 15c-.58.58-1.59 1-2.4 1H0v-2h2c.29 0 .8-.2 1-.41l2.17-2.18 1.42 1.42zM16 4V1l4 4-4 4V6h-2c-.29 0-.8.2-1 .41l-2.17 2.18L9.4 7.17 11.6 5c.58-.58 1.59-1 2.41-1h2zm0 10v-3l4 4-4 4v-3h-2c-.82 0-1.83-.42-2.41-1l-8.6-8.59C2.8 6.21 2.3 6 2 6H0V4h2c.82 0 1.83.42 2.41 1l8.6 8.59c.2.2.7.41.99.41h2z"
+                      />
+                    </svg>
+                  </div>
+                  <div class="text-grey-darker">
+                    <svg
+                      class="w-8 h-8"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M4 5h3v10H4V5zm12 0v10l-9-5 9-5z" />
+                    </svg>
+                  </div>
+                  <div
+                    class="cursor-pointer text-white p-8 rounded-full bg-red-700 shadow-lg"
+                    @click="toggleSong"
+                  >
+                    <svg
+                      class="w-8 h-8"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M5 4h3v12H5V4zm7 0h3v12h-3V4z" />
+                    </svg>
+                  </div>
+                  <div class="text-grey-darker">
+                    <svg
+                      class="w-8 h-8"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M13 5h3v10h-3V5zM4 5l9 5-9 5V5z" />
+                    </svg>
+                  </div>
+                  <div class="text-grey-darker">
+                    <svg
+                      class="w-8 h-8"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        d="M5 4a2 2 0 0 0-2 2v6H0l4 4 4-4H5V6h7l2-2H5zm10 4h-3l4-4 4 4h-3v6a2 2 0 0 1-2 2H6l2-2h7V8z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="mx-8 py-4">
+              <div class="flex justify-between text-sm text-grey-darker">
+                <p>{{ timer }}</p>
+                <p>{{ duration }}</p>
+              </div>
+              <div class="mt-1">
+                <div
+                  class="h-1 bg-gray-900 rounded-full"
+                  @click="moveTo($event)"
+                >
+                  <div
+                    class="h-1 bg-red-700 rounded-full relative"
+                    :style="{ width: `${progress}%` }"
+                  >
+                    <span
+                      class="-mt-2 absolute bg-red-700 h-5 right-0 rounded-full shadow top-0 w-5"
+                    ></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { Howl } from "howler";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      sound: null,
+      timer: "0:00",
+      progress: 0
+    };
+  },
+  computed: {
+    duration() {
+      if (!this.sound) {
+        return 0;
+      }
+
+      return this.formatTime(Math.round(this.sound.duration()));
+    }
+  },
+  created() {
+    this.sound = new Howl({
+      src: ["/mp3/yann_tiersen.mp3"],
+      onplay: () => {
+        requestAnimationFrame(this.step);
+      }
+    });
+  },
+  methods: {
+    toggleSong() {
+      if (!this.sound.playing()) {
+        this.sound.play();
+        return;
+      }
+
+      this.sound.pause();
+    },
+    formatTime: function(secs) {
+      var minutes = Math.floor(secs / 60) || 0;
+      var seconds = secs - minutes * 60 || 0;
+
+      return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    },
+    step() {
+      var seek = this.sound.seek() || 0;
+      this.timer = this.formatTime(Math.round(seek));
+      this.progress = (seek / this.sound.duration()) * 100 || 0;
+
+      // If the sound is still playing, continue stepping.
+      if (this.sound.playing()) {
+        requestAnimationFrame(this.step);
+      }
+    },
+    moveTo(event) {
+      // progress bar point half width
+      const element = event.currentTarget;
+
+      const pointerWidth = 10;
+      const mouseXPosition = event.pageX;
+      const progressDOMPosition = element.getBoundingClientRect().left;
+      const progressWidth = element.offsetWidth;
+
+      const percent =
+        (mouseXPosition - progressDOMPosition + pointerWidth) / progressWidth;
+
+      this.sound.seek(this.sound.duration() * percent);
+
+      if (!this.sound.playing()) {
+        this.step();
+      }
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
